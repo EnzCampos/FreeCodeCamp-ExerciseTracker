7a34fe1a41afa0f4fe2c9a4c0f1156b9e61851af
Enzo Henrique Ferreira Campos <98186034+EnzCampos@users.noreply.github.com>
2022-10-17 19:35:07 -0300

    Update mongoSchemas.js

--- mongoSchemas.js
+++ mongoSchemas.js
@@ -1,31 +1,33 @@
 let mongoose = require('mongoose')
 let express = require("express")
 
+
+let exerciseSchema = new mongoose.Schema({
+  userId: { 
+    type: String, 
+    required: true
+  },
+  description: { 
+    type: String, 
+    required: true 
+  },
+  duration: { 
+    type: Number, 
+    min: 1,
+    required: true
+  },
+  date: { 
+    type: Date, 
+    default: (new Date())
+  }
+}, { versionKey: false })
+
 let userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
     unique: true
-  } 
-}, { versionKey: false })
-
-let exerciseSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  duration : {
-    type: Number,
-    required: true
-  },
-  date: {
-    type: String,
-    default: (new Date()).toDateString()
-  } 
+  }
 }, { versionKey: false })
 
 const User = mongoose.model("User", userSchema);
