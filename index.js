c68ab077d6a0d8dafafca6780e3fc41ebff2a171
Enzo Henrique Ferreira Campos <98186034+EnzCampos@users.noreply.github.com>
2022-10-17 19:35:52 -0300

    Finished project code

--- index.js
+++ index.js
@@ -10,50 +10,119 @@ app.use(express.urlencoded({extended:false}))
 
 app.use(cors())
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.post('/api/users', async (req,res)=>{
-  try {
-    await User.create({ username:req.body.username })
-    const id = await User.findOne({ username:req.body.username }).select('-__v')
-    return res.json(id)
-  } catch (error) {
-    console.log(error);
+app.post('/api/users', (req,res) => {
+  let username = req.body.username
+  if (!username){
+    return res.json({error: 'Username is required'})
   }
-})
-
-
-app.get('/api/users', async (req,res) => {
-  try {
-    const userList = await User.find().select('-__v');
-    return res.json(userList);
-  } catch (error) {
-    console.log(error);
-  }
-})
-
-app.post('/api/users/:id/exercises', async (req,res) => {
-  const userFound = await User.findOne({
-    "_id":req.body[':_id']});
   
-  if (userFound) {
-    const userName = userFound.username;
-    const exerciseDate = req.body.date ? date : undefined;
-    try {
-      await Exercise.create({
-        username: userName,
-        description: req.body.description,
-        duration: Number(req.body.duration),
-        date: exerciseDate
-      })
-      return res.json( await Exercise.findOne({duration: Number(req.body.duration) }))
-    } catch (error) {
-      return console.log(error)
+  User.findOne({ username: username}, (error, foundUser) => {
+   if (!error && !foundUser) {
+     let newUser = new User({
+       username: username
+     })
+     
+     newUser.save((error,data)=>{
+       if (!error) {
+         return res.json({
+           _id: data['id'],
+           username: username
+         })
+       }
+     })
+   } else return res.json({ error: "Username already exists"})
+  })
+})
+
+app.get('/api/users', (req,res)=>{
+  User.find({}, (error,foundUsers) => {
+    if (!error) {
+      return res.json(foundUsers)
     }
+  })
+})
+
+app.post("/api/users/:_id/exercises", (req,res) => {
+  let { description, duration } = req.body
+  let date = req.body.date ? new Date(req.body.date) : new Date()
+  let userId = req.params._id
+  
+  if (!userId || !description || isNaN(duration) || date == "Invalid Date") {
+    return res.json({error: "Invalid Parameters"})
   }
-  return res.json({error: "Invalid User"})
+
+  User.findById( userId, (error,data) => {
+    if (!error) {
+      let newExercise = new Exercise({
+        userId: userId,
+        description: description,
+        duration: duration,
+        date: date
+      });
+
+      newExercise.save((err,savedData) => {
+        if (!err) {
+          return res.json({
+            _id: data['_id'],
+            username: data['username'],
+            description: savedData['description'],
+            duration: savedData['duration'],
+            date: date.toDateString()
+          });
+        }
+      });
+    }
+    else return res.json({error: "User not found"})
+  })
+})
+
+
+app.get('/api/users/:_id/logs', async (req,res) => {
+
+  let { from, to , limit} = req.query;
+  let searchParams = { userId: req.params._id };
+  
+  if (from || to) {
+    searchParams['date'] = {}
+  }
+  
+  if (from) {
+    searchParams.date.$gte = new Date(from);
+  }
+
+  if (to) {
+    searchParams.date.$lte = new Date(to);
+  }
+
+  
+  try {
+    
+    let user = await User.findById(req.params['_id']);
+    let log = await Exercise.find(searchParams, 'description duration date -_id').limit(limit ? limit : 0);
+
+    let result = {
+      username: user.username,
+      _id: req.params._id,
+      count: log.length,
+      log: log.map((exercise) => {
+        return {
+          description: exercise.description,
+          duration: exercise.duration,
+          date: exercise.date.toDateString()
+        }
+      })
+    }
+    
+    return res.json(result);
+    
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 const start = async () => {
